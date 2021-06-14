1c9fba1abd736b493728febed5427fac602894dd
Klaus Weinbauer <weinbauer.klaus@gmail.com>
2021-06-14 08:37:57 +0200

    add agent tests

--- src/integration-test/tests/test_agent.py
+++ src/integration-test/tests/test_agent.py
@@ -1,6 +1,8 @@
 import requests
 from copy import copy
 
+from requests.api import head
+
 
 DEFAULT_BODY = {
     "status": "idle",
@@ -9,12 +11,36 @@ DEFAULT_BODY = {
     "current_job": ""
 }
 
-def test_unauthorized(service):
+def test_unauthorized_update(service, agent_token, provider_token, admin_token):
     res = requests.put(service + "/agent/self", json=DEFAULT_BODY)
     assert res.status_code == 401
 
-def test_authorized(service, agent_token):
+    res = requests.get(service + "/auth/user/self", headers=agent_token)
+    agent_id = res.json().get("id")
+    res = requests.put(service + f"/agent/{agent_id}", json=DEFAULT_BODY, headers=provider_token)
+    assert res.status_code == 403
+
+    res = requests.put(service + f"/agent/{agent_id}", json=DEFAULT_BODY, headers=admin_token)
+    assert res.status_code == 403
+
+    res = requests.get(service + "/agent/self")
+    assert res.status_code == 401
+
+def test_unauthorized_get(service):
+    res = requests.get(service + "/agent/self")
+    assert res.status_code == 401
+
+def test_authorized(service, agent_token, admin_token, provider_token):
     res = requests.put(service + "/agent/self", json=DEFAULT_BODY, headers=agent_token)
+    assert res.status_code == 200
+
+    res = requests.get(service + "/auth/user/self", headers=agent_token)
+    agent_id = res.json().get("id")
+
+    res = requests.get(service + f"/agent/{agent_id}", headers=admin_token)
+    assert res.status_code == 200
+
+    res = requests.get(service + f"/agent/{agent_id}", headers=provider_token)
     assert res.status_code == 200
 
 def test_claim_job(service, agent_token, created_job):
@@ -34,8 +60,16 @@ def test_claim_job(service, agent_token, created_job):
     assert res.status_code == 200
 
     new_status["status"] = "off_duty"
+    new_status["longitude"] = 0
+    new_status["latitude"] = 0
     res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
     assert res.status_code == 200
+
+    res = requests.get(service + "/agent/self", headers=agent_token)
+    assert res.status_code == 200
+    assert res.json().get("status") == "off_duty"
+    assert res.json().get("longitude") == 0
+    assert res.json().get("latitude") == 0
 
 def test_invalid_states(service, agent_token, created_job):
     new_status = copy(DEFAULT_BODY)
@@ -44,3 +78,32 @@ def test_invalid_states(service, agent_token, created_job):
     res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
     assert res.status_code == 400
 
+def test_invalid_coordinates(service, agent_token):
+    new_status = copy(DEFAULT_BODY)
+    new_status["status"] = "invalid"
+    new_status["longitude"] = -181
+    res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
+    assert res.status_code == 400
+
+    new_status["longitude"] = 181
+    res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
+    assert res.status_code == 400
+
+    new_status["longitude"] = 0
+    new_status["latitude"] = -91
+    res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
+    assert res.status_code == 400
+
+    new_status["latitude"] = 91
+    res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
+    assert res.status_code == 400
+
+def test_invalid_job(service, agent_token):
+    new_status = copy(DEFAULT_BODY)
+    new_status["current_job"] = "unknown_job"
+    res = requests.put(service + "/agent/self", json=new_status, headers=agent_token)
+    assert res.status_code == 400
+
+def test_invalid_agent(service, agent_token):
+    res = requests.put(service + "/agent/unknown", json=DEFAULT_BODY, headers=agent_token)
+    assert res.status_code == 403
