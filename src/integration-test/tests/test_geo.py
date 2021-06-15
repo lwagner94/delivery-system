e9534732d6fa5ee300d722d526b087d5815fbf40
Klaus Weinbauer <weinbauer.klaus@gmail.com>
2021-06-15 10:38:56 +0200

    add geo test

--- src/integration-test/tests/test_geo.py
+++ src/integration-test/tests/test_geo.py
@@ -10,8 +10,12 @@ def test_geolocate_basic(service, admin_token, provider_token, agent_token):
         assert res.json().get("longitude") is not None
         assert res.json().get("latitude") is not None
 
-def test_geolocate_unauthorized(service, admin_token):
+def test_geolocate_unauthorized(service):
     params = {"address": "Inffeldgasse 16a, 8010 Graz"}
     res = requests.get(service + "/geo/coordinates", params=params)
     assert res.status_code == 401
 
+def test_geolocate_invalid(service, admin_token):
+    params = {"addresss": "NotARealAddress"}
+    res = requests.get(service + "/geo/coordinates", params=params, headers=admin_token)
+    assert res.status_code == 400
